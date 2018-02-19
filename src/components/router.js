f187216933270d1fe9fb482e8b96067f4a7e7ef6
Ivan Ermolaev <ermolaevym@gmail.com>
2018-02-20 02:11:22 +0300

    init

---
+++ src/components/router.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Router extends Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Router;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    //Content
+    entry: ['babel-polyfill', 'react-hot-loader/patch', './src/main.js'],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        publicPath: "/"
+    },
+    devServer: {
+        port: 3000,
+        host: 'localhost'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: "index.html",
+            template: "./src/index.tpl.html"
+        })
+    ]
+};
